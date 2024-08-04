39135c949caec79d5e926da4e1c529827f630423
vishnu-git1524 <vishnuvenkat1524@gmail.com>
2024-08-04 10:18:42 +0530

    updated animations

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,16 @@
 import "./mobile-menu.js";
+import TypeIt from "typeit";
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    new TypeIt(".hero_title", {
+      strings: ["Developer", "Programmer", "Coder"], // List of titles
+      speed: 100, // Typing speed
+      deleteSpeed: 75, // Deleting speed
+      breakLines: false,
+      loop: true,
+      nextStringDelay: 1000, // Delay before typing the next string
+      deleteDelay: 1000, // Delay before deleting the string
+    })
+    .go();
+  });
--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,6 +1,15 @@
 const mobileMenu = document.getElementById("mobileMenu");
 const menuToggler = document.getElementById("menuToggler");
+const navLinks = document.querySelectorAll(".navbar_links a");
 
-menuToggler.addEventListener("click", (e) => {
+menuToggler.addEventListener("click", () => {
   mobileMenu.classList.toggle("show");
+  menuToggler.classList.toggle("active");
+});
+
+navLinks.forEach(link => {
+  link.addEventListener("click", () => {
+    mobileMenu.classList.remove("show");
+    menuToggler.classList.remove("active");
+  });
 });
